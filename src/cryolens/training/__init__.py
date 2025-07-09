6a88959290b4ab20162da98cafd2d1a65f95a6f3
Kyle Harrington <czi@kyleharrington.com>
2025-07-09 16:27:19 -0700

    Add exports to training __init__.py including AdaptiveContrastiveAffinityLoss

---
+++ src/cryolens/training/__init__.py
@@ -0,0 +1,15 @@
+from .losses import (
+    MissingWedgeLoss, 
+    NormalizedMSELoss, 
+    ContrastiveAffinityLoss, 
+    AffinityCosineLoss,
+    AdaptiveContrastiveAffinityLoss
+)
+
+__all__ = [
+    'MissingWedgeLoss',
+    'NormalizedMSELoss', 
+    'ContrastiveAffinityLoss',
+    'AffinityCosineLoss',
+    'AdaptiveContrastiveAffinityLoss'
+]
